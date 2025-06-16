3617d332e287ebfde6da38c8a8872d9050b69ce9
kwanik-kor <rhksdlr134@naver.com>
2025-06-16 16:45:39 +0900

    chore: 기본 Kotlin 프로젝트 생성 및 Controller 생성

---
+++ kopring-exercise/src/main/kotlin/com/gani/kopring/KopringExerciseApplication.kt
@@ -0,0 +1,11 @@
+package com.gani.kopring
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KopringExerciseApplication
+
+fun main(args: Array<String>) {
+    runApplication<KopringExerciseApplication>(*args)
+}
---
+++ kopring-exercise/src/test/kotlin/com/gani/kopring/KopringExerciseApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.gani.kopring
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KopringExerciseApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
