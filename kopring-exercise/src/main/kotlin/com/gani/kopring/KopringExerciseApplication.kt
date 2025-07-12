17293348ad2a7fb4c4a321c6738758c5b4079863
kwanik-kor <rhksdlr134@naver.com>
2025-07-12 20:58:44 +0900

    chore: open class 전환

--- kopring-exercise/src/main/kotlin/com/gani/kopring/KopringExerciseApplication.kt
+++ kopring-exercise/src/main/kotlin/com/gani/kopring/KopringExerciseApplication.kt
@@ -4,7 +4,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class KopringExerciseApplication
+open class KopringExerciseApplication
 
 fun main(args: Array<String>) {
     runApplication<KopringExerciseApplication>(*args)
