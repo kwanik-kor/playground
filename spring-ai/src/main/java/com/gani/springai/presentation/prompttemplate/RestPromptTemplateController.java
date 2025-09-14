cad722b60ac2aa5afd13d6a52392d06993b3a114
kwanik-kor <rhksdlr134@naver.com>
2025-09-14 20:23:49 +0900

    feat: 다양한 프롬프트 엔지니어링 기법 알아보기

--- spring-ai/src/main/java/com/gani/springai/presentation/prompttemplate/RestPromptTemplateController.java
+++ spring-ai/src/main/java/com/gani/springai/presentation/prompttemplate/RestPromptTemplateController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -33,7 +34,61 @@ public class RestPromptTemplateController {
             session.setAttribute("chatMemory", chatMemory);
         }
 
-        String answer = promptTemplateService.multiMessage(question, chatMemory);
-        return answer;
+        return promptTemplateService.multiMessage(question, chatMemory);
     }
+
+    @PostMapping(
+            value = "/zero-shot-prompt",
+            consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE,
+            produces = MediaType.TEXT_PLAIN_VALUE
+    )
+    public String zeroShotPrompt(@RequestParam("review") String review) {
+        return promptTemplateService.zeroShotPrompt(review);
+    }
+
+    @PostMapping(
+            value = "/few-shot-prompt",
+            consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public String fewShotPrompt(@RequestParam("order") String order) {
+        return promptTemplateService.fewShotPrompt(order);
+    }
+
+    @PostMapping(
+            value = "/role-assignment",
+            consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE,
+            produces = MediaType.APPLICATION_NDJSON_VALUE
+    )
+    public Flux<String> roleAssignment(@RequestParam("requirements") String requirements) {
+        return promptTemplateService.roleAssignment(requirements);
+    }
+
+    @PostMapping(
+            value = "/step-back-prompt",
+            consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE,
+            produces = MediaType.TEXT_PLAIN_VALUE
+    )
+    public String stepBackPrompt(@RequestParam("question") String question) throws Exception {
+        return promptTemplateService.stepBackPrompt(question);
+    }
+
+    @PostMapping(
+            value = "/chain-of-thought",
+            consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE,
+            produces = MediaType.APPLICATION_NDJSON_VALUE
+    )
+    public Flux<String> chainOfThought(@RequestParam("question") String question) {
+        return promptTemplateService.chainOfThought(question);
+    }
+
+    @PostMapping(
+            value = "/self-consistency",
+            consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE,
+            produces = MediaType.TEXT_PLAIN_VALUE
+    )
+    public String selfConsistency(@RequestParam("content") String content) {
+        return promptTemplateService.selfConsistency(content);
+    }
+
 }
